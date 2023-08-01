968d51442731dbf3a38b2879bebcdaca758c68f2
Dominick Confessore <domconfessore@gmail.com>
2023-08-01 11:47:18 -0400

    updated styling, added info to resumepage

--- src/components/AboutMePage.js
+++ src/components/AboutMePage.js
@@ -4,11 +4,11 @@ import photo from "../assets/images/Portfolio-Avatar.jpg";
 export default function AboutMe() {
     return (
         <div>
-            <h3>About Me</h3>
-            <div>
+            <h2 className="title-aboutme mb-3">About Me</h2>
+            <div className="pix">
                 <img className="image" src={photo} alt="Dom" />
             </div>
-            <div>
+            <div className="aboutme">
                 <p>
                     Hi there! My name is Dominick Confessore and I am an aspiring web developer who has recently finished the Rutgers Coding Bootcamp. Prior to pursuing a career in the coding world, I worked for the Somerset Patriots where I was the assistant groundskeeper for a season. I then dove into the car industry, where I was a service advisor for three years. After getting married, I decided it was time to pursue a new chapter in my work life. I pride myself in being an extremely hard worker and I cannot wait to continue my journey into the developer world!
                 </p>
@@ -16,3 +16,4 @@ export default function AboutMe() {
         </div>
     )
 }
+
--- src/components/ResumePage.js
+++ src/components/ResumePage.js
@@ -1,9 +1,53 @@
 import React from "react";
+import ResumeDoc from "../assets/VanessaVillaluzResume.pdf";
+
+const frontEndSkills = [
+    "HTML",
+    "CSS",
+    "Bootstrap",
+    "JavaScript",
+    "jQuery",
+    "React"
+];
+
+const backEndSkills = [
+    "Node.js",
+    "Express.js",
+    "MySQL",
+    "Mongoose ODM",
+    "MongoDB",
+];
 
 export default function Resume() {
     return (
-        <div>
-            <h3>Resume info coming..</h3>
+        <div className="flex-row mt-4">
+            <div className="row justify-content-center">
+                <h3 className="row resume-title">Resume</h3>
+                <h4 className="resume row">
+                    Download Dominick's {" "} <a href={ResumeDoc} download="" className="row justify-content-center"> Resume
+                    </a>
+                </h4>
+            </div>
+            <div>
+                <h5 className="row justify-content-center mt-3">Front-End Experience</h5>
+                <ul className="experience">
+                    {frontEndSkills.map((frontEndSkills, index) => (
+                        <li className="experience" key={index}>
+                            {frontEndSkills}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <h5 className="row justify-content-center mt-3">Back-End Experience</h5>
+                <ul className="experience">
+                    {backEndSkills.map((backEndSkills, index) => (
+                        <li className="experience" key={index}>
+                            {backEndSkills}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
