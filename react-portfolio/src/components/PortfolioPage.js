f5466b7078ba7c02d3e754ee5811fc09f6fb9c74
Dominick Confessore <domconfessore@gmail.com>
2023-08-02 16:23:57 -0400

    fixed react app issue

--- react-portfolio/src/components/PortfolioPage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Project from "./Project"
-import projects from "./projectsDone"
-
-export default function Portfolio() {
-    return (
-        <Project projects={projects} />
-    )
-}
